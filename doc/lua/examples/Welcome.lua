9e7f51df70949b221bb38a40ab0cfaa6c99b9dfc
David Demelier <markand@malikania.fr>
2013-06-08 10:08:44 +0200

    Lot of changes

--- doc/lua/examples/Welcome.lua
+++
@@ -1,8 +0,0 @@
--- Welcome.lua
--- This little script wait for someone to come on a channel and
--- welcome it
-
--- Welcome someone on a channel
-function onJoin(server, channel, nickname)
-	server:say(channel, "Welcome " .. nickname .. "!")
-end
