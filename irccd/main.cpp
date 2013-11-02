60b8a39737255df68a8971b78af9d0c457a99061
David Demelier <markand@malikania.fr>
2013-11-02 17:27:56 +0100

    Update licenses
    Task            #206
    MFD after       1 week

--- irccd/main.cpp
+++ irccd/main.cpp
@@ -1,7 +1,7 @@
 /*
  * main.cpp -- irccd main file
  *
- * Copyright (c) 2011, 2012, 2013 David Demelier <markand@malikania.fr>
+ * Copyright (c) 2013 David Demelier <markand@malikania.fr>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
