3cb105d31e81c20f5367e6e79fa1acfb2cd60a7e
David Demelier <markand@malikania.fr>
2013-12-16 22:24:30 +0100

    Update examples

--- doc/ldoc/lua/examples/Welcome.lua
+++ doc/ldoc/lua/examples/Welcome.lua
@@ -2,7 +2,9 @@
 -- This little script wait for someone to come on a channel and
 -- welcome it
 
+local util = require "irccd.util"
+
 -- Welcome someone on a channel
 function onJoin(server, channel, nickname)
-	server:say(channel, "Welcome " .. nickname .. "!")
+	server:say(channel, "Welcome " .. util.splituser(nickname) .. "!")
 end
