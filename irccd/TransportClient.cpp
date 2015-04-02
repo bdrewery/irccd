97a439139cf88848bb3eaab72a94418065f3e1c9
David Demelier <markand@malikania.fr>
2015-04-02 21:16:33 +0200

    Add license headers

--- irccd/TransportClient.cpp
+++ irccd/TransportClient.cpp
@@ -1,8 +1,25 @@
+/*
+ * TransportClient.cpp -- client connected to irccd
+ *
+ * Copyright (c) 2013, 2014, 2015 David Demelier <markand@malikania.fr>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <Logger.h>
+
 #include "SocketListener.h"
 #include "TransportClient.h"
-
-// TMP
-#include <iostream>
 
 namespace irccd {
 
@@ -29,7 +46,7 @@ void TransportClientAbstract::receive()
 		try {
 			m_onComplete(message);
 		} catch (const std::exception &ex) {
-			// TODO ERROR
+			Logger::warning() << "transport: " << ex.what() << std::endl;
 		}
 	}
 }
